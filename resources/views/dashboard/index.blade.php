fc5f506ea474fc54ccea1149af8985f7f5f63a74
Hamid Machfudin Sukardi <hamidmsukardi@gmail.com>
2023-07-10 20:02:54 +0700

    refactor

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -5,8 +5,4 @@
 @section('content')
     <h1>Dashboard</h1>
     <p>Welcome, {{ Auth::user()->name }}!</p>
-    <form action="{{ route('logout') }}" method="POST">
-        @csrf
-        <button type="submit">Logout</button>
-    </form>
 @endsection
